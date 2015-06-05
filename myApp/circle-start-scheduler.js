b6bf0f6fed022d58c7d6fddd9bc8d8d7e5855624
deepeshsunku <deepesh.sunku@gmail.com>
2015-06-05 10:40:08 -0400

    Scheduler Implementation
    This commit has the following added:
    1. Nodejs Scheduler for tracking start of a circle
    2. Support for a recurring recurring nodejs scheduler for weekly/
    biweekly/ monthly plans.

--- myApp/circle-start-scheduler.js
+++ myApp/circle-start-scheduler.js
@@ -8,24 +8,76 @@ var fbRef = new Firebase("https://walletbuddies.firebaseio.com/");
 fbRef.child('StartDate').on('child_added', function(dateSnap) {
 	var dateUnformatted = dateSnap.val();
 	var date = new Date(dateUnformatted.date);
-	var month = date.getMonth() + 1;
-	console.log("FORMATTED DATE: " + date.getFullYear() + "-" + date.getDate() + "-" +  month + " / " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+	console.log("FORMATTED DATE: " + date.getFullYear() + "-" + date.getDate() + "-" +  date.getMonth() + " / " + date.getHours() + ":" + date.getMinutes() + ":" + 			date.getSeconds());
 	
 	// Remove it now that we've processed the date
     dateSnap.ref().remove();
 	
-	// Schedule a job at specified date
+	// Schedule a job at specified date (Currently set to 5 mins from circle creation)
 	var mins = date.getMinutes() + 2;
-	var scheduledDate = new Date(date.getFullYear(), month, date.getDate(), date.getHours(), mins, date.getSeconds());
-	//month = scheduledDate.getMonth() + 1;
-	console.log("Scheduled DATE: " + scheduledDate.getFullYear() + "-" + scheduledDate.getDate() + "-" +  scheduledDate.getMonth() + " / " + scheduledDate.getHours() + ":" + scheduledDate.getMinutes() + ":" + scheduledDate.getSeconds());
-	var j = schedule.scheduleJob(scheduledDate, function(){
-    	console.log('SUCESSS BITCHES');
-    	var time = Firebase.ServerValue.TIMESTAMP;
-    	fbRef.child('SUCCESS').push({
-	    	Bitches: time
-    	});
-	});
-	console.log("THE END");
+	var scheduledDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), mins, date.getSeconds());
+	console.log("Scheduled DATE: " + scheduledDate.getFullYear() + "-" + scheduledDate.getDate() + "-" +  scheduledDate.getMonth() + " / " + scheduledDate.getHours() + ":" + 	scheduledDate.getMinutes() + ":" + scheduledDate.getSeconds());
 	
+	// Start the job at the scheduled date
+	var j = schedule.scheduleJob(scheduledDate, function(){
+		// Get a reference to the circle's path
+		var path = new Firebase("https://walletbuddies.firebaseio.com/").child("Circles").child(dateUnformatted.circleID).child("Members");
+		
+		// Check for and remove user's with a "pending" status
+		path.orderByChild("Status").equalTo("pending").on('child_added', function(data){
+			console.log("SERVICE: " + data.key());
+			path.child(data.key()).remove();
+		});
+		
+		// Check for and remove user's with a "false" status
+		path.orderByChild("Status").equalTo(false).on('child_added', function(data){
+			console.log("SERVICE: " + data.key());
+			path.child(data.key()).remove();
+		});
+		
+		// Generate and assign a random priority for the remaining users
+		path.once('value', function(data) {
+			console.log("No of Children: " + data.numChildren());
+			if (data.numChildren() < 3) {
+				// Write code to inform user and delete circle
+			}
+			else {
+				// Generate unique random numbers
+				var limit = data.numChildren(),
+			    lower_bound = 0,
+			    upper_bound = limit-1,
+			    unique_random_numbers = [];
+			
+				while (unique_random_numbers.length < limit) {
+			    	var random_number = Math.round(Math.random()*(upper_bound - lower_bound) + lower_bound);
+					if (unique_random_numbers.indexOf(random_number) == -1) { 
+				        // Yay! new unique random number
+				        console.log(random_number);
+				        unique_random_numbers.push( random_number );
+			    	}
+				}
+				
+				// Assign a random priority for the remaining users
+				var count = 0;
+				data.forEach(function(childSnapshot) {
+					var key = childSnapshot.key();
+					console.log("KEYS: " + key + "Count" + count);
+					path.child(key).update({
+						Priority: unique_random_numbers[count]
+					});
+					count++;
+				});
+				
+				// Save the timestamp & circleID to trigger the payments-start-scheduler
+				var circleStartDate = Firebase.ServerValue.TIMESTAMP;
+				fbRef.child('PaymentStart').push({
+					date: circleStartDate,
+					circleID: dateUnformatted.circleID,
+					plan: dateUnformatted.plan,
+					length: data.numChildren()
+				});
+			}
+		});
+		
+	}.bind(null, dateUnformatted));	// This is done to use current data in the future
 });
---
+++ myApp/payment-start-scheduler.js
@@ -0,0 +1,61 @@
+var Firebase = require("firebase");
+var schedule = require('node-schedule');
+
+// Get a reference to the Firebase account
+var fbRef = new Firebase("https://walletbuddies.firebaseio.com/");
+
+// Monitor the folder /StartDate for new data
+fbRef.child('PaymentStart').on('child_added', function(dateSnap) {
+	var date = new Date(dateSnap.val().date);
+	var rule = new schedule.RecurrenceRule();
+	rule.minute = date.getMinutes();
+	//rule.hour = date.getHours();
+	
+	// Remove it now that we've processed the date
+    dateSnap.ref().remove();
+	
+	console.log("FORMATTED DATE: " + date.getFullYear() + "-" + date.getDate() + "-" +  date.getMonth() + " / " + date.getHours() + ":" + date.getMinutes() + ":" + 			date.getSeconds());
+	console.log("PLAN: " + dateSnap.val().plan);
+	
+	// Set day of week/month rule based on circle plan
+	if (dateSnap.val().plan == "weekly") {
+		//rule.dayOfWeek = date.getDay(); // Returns the day of the week (0-6) for the specified date according to local time
+		rule.minute = new schedule.Range(0, 60, 1); // For testing purposes let's run the job every 2 mins for weekly plan
+	}
+	else if (dateSnap.val().plan == "monthly") {
+		if (date.getDate() <= 28){
+			//rule.dayOfMonth = date.getDate(); // Returns the day of the month (1-31) for the specified date according to local time
+			rule.minute = new schedule.Range(0, 60, 6);; // For testing purposes let's run the job every 6 mins for monthly plan
+		}
+		else
+		{
+			//rule.dayOfMonth = 1; // Push job date to 1st for avoiding a skip in february
+			rule.minute = new schedule.Range(0, 60, 6); // For testing purposes let's run the job every 6 mins for monthly plan
+		}
+	}
+	else {
+		//rule.dayOfMonth = [1, 15]; // For biweekly plans the job runs on the 1st and 15th of every month
+		rule.minute = new schedule.Range(0, 1, 15); // For testing purposes let's run the job every 1 and 15 mins for biweekly plan
+	}
+	console.log("CRON DATE: " + rule.minute);
+	var count = 0;
+	// Run the job at specified cronological date
+	var j = schedule.scheduleJob(rule, function(){
+	    console.log('Starting CRON');
+	    // Get a reference to the Firebase account
+		var path = new Firebase("https://walletbuddies.firebaseio.com/").child("Circles").child(dateSnap.val().circleID).child("Members");
+	    if (count < dateSnap.val().length){
+		    // Check for and remove user's with a "pending" status
+			path.orderByChild("Priority").equalTo(count).on('child_added', function(data){
+				console.log("Payment made to Mr. " + data.key() + " with Priority " + count-1);
+			});
+			// Increment the counter
+		    count++;
+	    }
+	    else {
+		    console.log("CANCELLING CRON :(...Priority has hit " + count);
+		    j.cancel();
+	    }
+	    
+	}.bind(null, dateSnap, count));
+});
